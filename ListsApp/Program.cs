2a3a79eec68222268a7549dbc581d7eaf98ef927
kaloyan <kaloyan.kolev@hotmail.com>
2024-09-13 12:39:10 +0300

    #add Remove list items

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -9,12 +9,31 @@
             colours.Add("red");
             colours.Add("blue");
             colours.Add("green");
+            colours.Add("red");
             
             Console.WriteLine("Current colours");
             foreach (string colour in colours)
             {
                 Console.WriteLine(colour);
             }
+
+            bool deleteSuccesful = colours.Remove("red");
+
+            while (deleteSuccesful)
+            {
+                deleteSuccesful = colours.Remove("red");
+            }
+
+            Console.WriteLine("Current colours");
+            foreach (string colour in colours)
+            {
+                Console.WriteLine(colour);
+            }
+
+
+
+
+
             Console.ReadKey();
         }
     }
