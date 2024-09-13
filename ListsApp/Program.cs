3209d43095e6b4a6f73987539029fe30c189499c
kaloy <kaloy@MSI>
2024-09-13 12:22:19 +0300

    Add project files.

---
+++ ListsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ListsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> colours = new List<string>();
+            colours.Add("red");
+            colours.Add("blue");
+            colours.Add("green");
+            
+            Console.WriteLine("Current colours");
+            foreach (string colour in colours)
+            {
+                Console.WriteLine(colour);
+            }
+            Console.ReadKey();
+        }
+    }
+}
