3f6b45099c92458ecbb68678588b728ad9afa784
kaloyan <kaloyan.kolev@hotmail.com>
2024-09-17 14:41:41 +0300

    Constructor inheritance

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -3,57 +3,41 @@
 namespace ListsApp
 {
     internal class Program
+
     {
+        public class Person
+        {
+
+            private string Name { get; set; }
+            public int Age { get; set; }
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+                Console.WriteLine("Person constructor called");
+            }
+
+            public void DisplayPersonInfo()
+            {
+                Console.WriteLine($"Name: {Name}, Age: {Age}");
+            }
+        }
+
+        public class  Employee:Person
+        {
+            public Employee(string name, int age) : base(name,age)
+            {
+                Console.WriteLine("Employee constructor called: derived");
+            }
+        }
+
+
         static void Main(string[] args)
         {
-
-            Dog dog = new Dog();
-            Collie collie = new Collie();
-            Cat cat = new Cat();
-
-            dog.MakeSound();
-            cat.MakeSound();
-
+            Employee joe = new Employee("Joe", 35);
+            joe.DisplayPersonInfo();
             Console.ReadKey();
 
-        }
-
-        class Animal
-        {
-            public void Eat()
-            {
-                Console.WriteLine("Eating...");
-            }
-
-            public virtual void MakeSound()
-            {
-                Console.WriteLine("Generic sound");
-            }
-        }
-
-        class Dog: Animal
-        {
-            public override void MakeSound()
-            {
-                base.MakeSound();
-                Console.WriteLine("Barking");
-            }
-        }
-
-        class Collie: Dog
-        {
-            public void GoingNuts()
-            {
-                Console.WriteLine("Going Nuts");
-            }
-        }
-
-        class Cat: Animal
-        {
-            public override void MakeSound()
-            {
-                Console.WriteLine("Meowing");
-            }
         }
 
     }
