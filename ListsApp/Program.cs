d09bb0d86623206af77cf172e458e264f1a015a5
kaloyan <kaloyan.kolev@hotmail.com>
2024-09-17 15:03:14 +0300

    Constructor inheritance with additional properties

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -25,17 +25,27 @@ namespace ListsApp
 
         public class  Employee:Person
         {
-            public Employee(string name, int age) : base(name,age)
+            public string JobTitle { get; set; }
+            public int EmloyeedID { get; set; }
+            public Employee(string name, int age, string jobTitle, int emloyeedID) : base(name, age)
             {
                 Console.WriteLine("Employee constructor called: derived");
+                JobTitle = jobTitle;
+                EmloyeedID = emloyeedID;
+            }
+
+            public void DisplayEmployeeInfo()
+            {
+                DisplayPersonInfo();
+                Console.WriteLine($"Job title: {JobTitle}, EmployeeID {EmloyeedID}");
             }
         }
 
 
         static void Main(string[] args)
         {
-            Employee joe = new Employee("Joe", 35);
-            joe.DisplayPersonInfo();
+            Employee joe = new Employee("Joe", 35,"Banker", 12421);
+            joe.DisplayEmployeeInfo();
             Console.ReadKey();
 
         }
