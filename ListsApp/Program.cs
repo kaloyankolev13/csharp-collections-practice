447ea4b6dc7d9c14d6461d384fc848ca02b28bfb
kaloyan <kaloyan.kolev@hotmail.com>
2024-09-17 13:37:39 +0300

    Inherticance

--- ListsApp/Program.cs
+++ ListsApp/Program.cs
@@ -2,35 +2,54 @@
 
 namespace ListsApp
 {
-    class Student
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Grade { get; set; }
-
-        public Student(int id, string name, int grade)
-        {
-            Id = id;
-            Name = name;
-            Grade = grade;
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Dictionary<string, Student> students = new Dictionary<string, Student>
-            {
-                ["John"] = new Student(1, "John", 85),
-                ["Alice"] = new Student(2, "Alice", 90),
-                ["Bob"] = new Student(3, "Bob", 78),
-            };
+            Dog dog = new Dog();
+            Collie collie = new Collie();
+            dog.Bark();
+            dog.Eat();
+            collie.Bark();
+            collie.GoingNuts();
+            Console.ReadKey();
 
-            foreach (var student in students) {
-                Console.WriteLine($"Name: {student.Value.Name}, Id: {student.Value.Id}, Grade: {student.Value.Grade}");
+        }
+
+        class Animal
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Eating...");
             }
         }
+
+        class Cat : Animal
+        {
+            public void Meow()
+            {
+                Console.WriteLine("Meowing");
+            }
+        }
+
+        class Dog: Animal
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Barking");
+            }
+        }
+
+        class Collie: Dog
+        {
+            public void GoingNuts()
+            {
+                Console.WriteLine("Going Nuts");
+            }
+        }
+
+
 
     }
 }
